ab1fdf180fe6ad8920e741df79cc470874b0bfdf
Malte <malte.lukas@fb-pro.com>
2025-05-05 09:11:06 +0200

    changed GDM rule 1.7.6 to only return exit code 1 or 0; in 1.7.4 check first if gnome is installed

--- ATAPAuditor/AuditGroups/Debian Linux 12-CIS-1.0.1.ps1
+++ ATAPAuditor/AuditGroups/Debian Linux 12-CIS-1.0.1.ps1
@@ -868,8 +868,8 @@ $FirewallStatus = GetFirewallStatus
     Test = {
         $parentPath = Split-Path -Parent -Path $PSScriptRoot
         $path = $parentPath+"/Helpers/ShellScripts/Ubuntu22.04_Debian12/1.8.6.sh"
-        $result=bash $path | grep " PASS "
-        if($result -ne $null){
+        $result=bash $path
+        if($?){
             return $retCompliant
         }
 
