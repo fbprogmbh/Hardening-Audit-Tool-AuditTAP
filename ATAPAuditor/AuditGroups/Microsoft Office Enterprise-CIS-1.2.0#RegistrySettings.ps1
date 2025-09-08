0db9c9d5f7e54af17911b0c0f64069b8d34958f5
RAW0611 <raw0611@gmail.com>
2025-09-08 11:57:19 +0200

    1.1.4.1.11 order rearranged

--- ATAPAuditor/AuditGroups/Microsoft Office Enterprise-CIS-1.2.0#RegistrySettings.ps1
+++ ATAPAuditor/AuditGroups/Microsoft Office Enterprise-CIS-1.2.0#RegistrySettings.ps1
@@ -7769,8 +7769,8 @@ else {
     }
 }
 [AuditTest] @{
-    Id   = "1.1.4.1.11 K"
-    Task = "(L1) Ensure 'Restrict ActiveX Install' is set to Enabled  (exprwd.exe)"
+    Id   = "1.1.4.1.11 A"
+    Task = "(L1) Ensure 'Restrict ActiveX Install' is set to Enabled (groove.exe)"
     Test = {
         # new logic: 
         # - if no Office installed at all -> skip test 
@@ -7783,7 +7783,7 @@ else {
                 Status  = "None"
             }
         }
-        elseif (-not $installedOfficeApps["Expression Web"]) {
+        elseif (-not $installedOfficeApps["Groove"]) {
             return @{
                 Message = "Application not installed, skipping test."
                 Status  = "None"
@@ -7793,173 +7793,8 @@ else {
             try {
                 $regValue = Get-ItemProperty -ErrorAction Stop `
                     -Path "Registry::HKEY_LOCAL_MACHINE\Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_RESTRICT_ACTIVEXINSTALL" `
-                    -Name "exprwd.exe" `
-                | Select-Object -ExpandProperty "exprwd.exe"
-
-                if (($regValue -ne 1)) {
-                    return @{
-                        Message = "Registry value is '$regValue'. Expected: x == 1"
-                        Status  = "False"
-                    }
-                }
-            }
-            catch [System.Management.Automation.PSArgumentException] {
-                return @{
-                    Message = "Registry value not found."
-                    Status  = "False"
-                }
-            }
-            catch [System.Management.Automation.ItemNotFoundException] {
-                return @{
-                    Message = "Registry key not found."
-                    Status  = "False"
-                }
-            }
-
-            return @{
-                Message = "Compliant"
-                Status  = "True"
-            }
-        }
-    }
-}
-[AuditTest] @{
-    Id   = "1.1.4.1.11 L"
-    Task = "(L1) Ensure 'Restrict ActiveX Install' is set to Enabled  (msaccess.exe)"
-    Test = {
-        # new logic: 
-        # - if no Office installed at all -> skip test 
-        # - if Office installed but app not installed -> skip test
-        # - else run test as normal
-
-        if (-not $OfficeInstalled) {
-            return @{
-                Message = "No Office installation detected, skipping test."
-                Status  = "None"
-            }
-        }
-        elseif (-not $installedOfficeApps["Access"]) {
-            return @{
-                Message = "Application not installed, skipping test."
-                Status  = "None"
-            }
-        }
-        else {
-            try {
-                $regValue = Get-ItemProperty -ErrorAction Stop `
-                    -Path "Registry::HKEY_LOCAL_MACHINE\Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_RESTRICT_ACTIVEXINSTALL" `
-                    -Name "msaccess.exe" `
-                | Select-Object -ExpandProperty "msaccess.exe"
-
-                if (($regValue -ne 1)) {
-                    return @{
-                        Message = "Registry value is '$regValue'. Expected: x == 1"
-                        Status  = "False"
-                    }
-                }
-            }
-            catch [System.Management.Automation.PSArgumentException] {
-                return @{
-                    Message = "Registry value not found."
-                    Status  = "False"
-                }
-            }
-            catch [System.Management.Automation.ItemNotFoundException] {
-                return @{
-                    Message = "Registry key not found."
-                    Status  = "False"
-                }
-            }
-
-            return @{
-                Message = "Compliant"
-                Status  = "True"
-            }
-        }
-    }
-}
-[AuditTest] @{
-    Id   = "1.1.4.1.11 M"
-    Task = "(L1) Ensure 'Restrict ActiveX Install' is set to Enabled  (onent.exe)"
-    Test = {
-        # new logic: 
-        # - if no Office installed at all -> skip test 
-        # - if Office installed but app not installed -> skip test
-        # - else run test as normal
-
-        if (-not $OfficeInstalled) {
-            return @{
-                Message = "No Office installation detected, skipping test."
-                Status  = "None"
-            }
-        }
-        elseif (-not $installedOfficeApps["OneNote"]) {
-            return @{
-                Message = "Application not installed, skipping test."
-                Status  = "None"
-            }
-        }
-        else {
-            try {
-                $regValue = Get-ItemProperty -ErrorAction Stop `
-                    -Path "Registry::HKEY_LOCAL_MACHINE\Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_RESTRICT_ACTIVEXINSTALL" `
-                    -Name "onent.exe" `
-                | Select-Object -ExpandProperty "onent.exe"
-
-                if (($regValue -ne 1)) {
-                    return @{
-                        Message = "Registry value is '$regValue'. Expected: x == 1"
-                        Status  = "False"
-                    }
-                }
-            }
-            catch [System.Management.Automation.PSArgumentException] {
-                return @{
-                    Message = "Registry value not found."
-                    Status  = "False"
-                }
-            }
-            catch [System.Management.Automation.ItemNotFoundException] {
-                return @{
-                    Message = "Registry key not found."
-                    Status  = "False"
-                }
-            }
-
-            return @{
-                Message = "Compliant"
-                Status  = "True"
-            }
-        }
-    }
-}
-[AuditTest] @{
-    Id   = "1.1.4.1.11 N"
-    Task = "(L1) Ensure 'Restrict ActiveX Install' is set to Enabled  (mse7.exe)"
-    Test = {
-        # new logic: 
-        # - if no Office installed at all -> skip test 
-        # - if Office installed but app not installed -> skip test
-        # - else run test as normal
-
-        if (-not $OfficeInstalled) {
-            return @{
-                Message = "No Office installation detected, skipping test."
-                Status  = "None"
-            }
-        }
-        elseif (-not $installedOfficeApps["MS Script Editor"]) {
-            return @{
-                Message = "Application not installed, skipping test."
-                Status  = "None"
-            }
-        }
-        else {
-            try {
-                $regValue = Get-ItemProperty -ErrorAction Stop `
-                    -Path "Registry::HKEY_LOCAL_MACHINE\Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_RESTRICT_ACTIVEXINSTALL" `
-                    -Name "mse7.exe" `
-                | Select-Object -ExpandProperty "mse7.exe"
+                    -Name "groove.exe" `
+                | Select-Object -ExpandProperty "groove.exe"
 
                 if (($regValue -ne 1)) {
                     return @{
@@ -8319,6 +8154,61 @@ else {
     }
 }
 [AuditTest] @{
+    Id   = "1.1.4.1.11 H"
+    Task = "(L1) Ensure 'Restrict ActiveX Install' is set to 'Enabled' (winword.exe)"
+    Test = {
+        # new logic: 
+        # - if no Office installed at all -> skip test 
+        # - if Office installed but app not installed -> skip test
+        # - else run test as normal
+
+        if (-not $OfficeInstalled) {
+            return @{
+                Message = "No Office installation detected, skipping test."
+                Status  = "None"
+            }
+        }
+        elseif (-not $installedOfficeApps["Word"]) {
+            return @{
+                Message = "Application not installed, skipping test."
+                Status  = "None"
+            }
+        }
+        else {
+            try {
+                $regValue = Get-ItemProperty -ErrorAction Stop `
+                    -Path "Registry::HKEY_LOCAL_MACHINE\software\microsoft\internet explorer\main\featurecontrol\feature_restrict_activexinstall" `
+                    -Name "winword.exe" `
+                | Select-Object -ExpandProperty "winword.exe"
+
+                if (($regValue -ne 1)) {
+                    return @{
+                        Message = "Registry value is '$regValue'. Expected: x == 1"
+                        Status  = "False"
+                    }
+                }
+            }
+            catch [System.Management.Automation.PSArgumentException] {
+                return @{
+                    Message = "Registry value not found."
+                    Status  = "False"
+                }
+            }
+            catch [System.Management.Automation.ItemNotFoundException] {
+                return @{
+                    Message = "Registry key not found."
+                    Status  = "False"
+                }
+            }
+
+            return @{
+                Message = "Compliant"
+                Status  = "True"
+            }
+        }
+    }
+}
+[AuditTest] @{
     Id   = "1.1.4.1.11 I"
     Task = "(L1) Ensure 'Restrict ActiveX Install' is set to Enabled  (outlook.exe)"
     Test = {
@@ -8429,8 +8319,8 @@ else {
     }
 }
 [AuditTest] @{
-    Id   = "1.1.4.1.11 A"
-    Task = "(L1) Ensure 'Restrict ActiveX Install' is set to Enabled (groove.exe)"
+    Id   = "1.1.4.1.11 K"
+    Task = "(L1) Ensure 'Restrict ActiveX Install' is set to Enabled  (exprwd.exe)"
     Test = {
         # new logic: 
         # - if no Office installed at all -> skip test 
@@ -8443,7 +8333,7 @@ else {
                 Status  = "None"
             }
         }
-        elseif (-not $installedOfficeApps["Groove"]) {
+        elseif (-not $installedOfficeApps["Expression Web"]) {
             return @{
                 Message = "Application not installed, skipping test."
                 Status  = "None"
@@ -8453,8 +8343,8 @@ else {
             try {
                 $regValue = Get-ItemProperty -ErrorAction Stop `
                     -Path "Registry::HKEY_LOCAL_MACHINE\Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_RESTRICT_ACTIVEXINSTALL" `
-                    -Name "groove.exe" `
-                | Select-Object -ExpandProperty "groove.exe"
+                    -Name "exprwd.exe" `
+                | Select-Object -ExpandProperty "exprwd.exe"
 
                 if (($regValue -ne 1)) {
                     return @{
@@ -8484,8 +8374,8 @@ else {
     }
 }
 [AuditTest] @{
-    Id   = "1.1.4.1.11 H"
-    Task = "(L1) Ensure 'Restrict ActiveX Install' is set to 'Enabled' (winword.exe)"
+    Id   = "1.1.4.1.11 L"
+    Task = "(L1) Ensure 'Restrict ActiveX Install' is set to Enabled  (msaccess.exe)"
     Test = {
         # new logic: 
         # - if no Office installed at all -> skip test 
@@ -8498,7 +8388,7 @@ else {
                 Status  = "None"
             }
         }
-        elseif (-not $installedOfficeApps["Word"]) {
+        elseif (-not $installedOfficeApps["Access"]) {
             return @{
                 Message = "Application not installed, skipping test."
                 Status  = "None"
@@ -8507,9 +8397,119 @@ else {
         else {
             try {
                 $regValue = Get-ItemProperty -ErrorAction Stop `
-                    -Path "Registry::HKEY_LOCAL_MACHINE\software\microsoft\internet explorer\main\featurecontrol\feature_restrict_activexinstall" `
-                    -Name "winword.exe" `
-                | Select-Object -ExpandProperty "winword.exe"
+                    -Path "Registry::HKEY_LOCAL_MACHINE\Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_RESTRICT_ACTIVEXINSTALL" `
+                    -Name "msaccess.exe" `
+                | Select-Object -ExpandProperty "msaccess.exe"
+
+                if (($regValue -ne 1)) {
+                    return @{
+                        Message = "Registry value is '$regValue'. Expected: x == 1"
+                        Status  = "False"
+                    }
+                }
+            }
+            catch [System.Management.Automation.PSArgumentException] {
+                return @{
+                    Message = "Registry value not found."
+                    Status  = "False"
+                }
+            }
+            catch [System.Management.Automation.ItemNotFoundException] {
+                return @{
+                    Message = "Registry key not found."
+                    Status  = "False"
+                }
+            }
+
+            return @{
+                Message = "Compliant"
+                Status  = "True"
+            }
+        }
+    }
+}
+[AuditTest] @{
+    Id   = "1.1.4.1.11 M"
+    Task = "(L1) Ensure 'Restrict ActiveX Install' is set to Enabled  (onent.exe)"
+    Test = {
+        # new logic: 
+        # - if no Office installed at all -> skip test 
+        # - if Office installed but app not installed -> skip test
+        # - else run test as normal
+
+        if (-not $OfficeInstalled) {
+            return @{
+                Message = "No Office installation detected, skipping test."
+                Status  = "None"
+            }
+        }
+        elseif (-not $installedOfficeApps["OneNote"]) {
+            return @{
+                Message = "Application not installed, skipping test."
+                Status  = "None"
+            }
+        }
+        else {
+            try {
+                $regValue = Get-ItemProperty -ErrorAction Stop `
+                    -Path "Registry::HKEY_LOCAL_MACHINE\Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_RESTRICT_ACTIVEXINSTALL" `
+                    -Name "onent.exe" `
+                | Select-Object -ExpandProperty "onent.exe"
+
+                if (($regValue -ne 1)) {
+                    return @{
+                        Message = "Registry value is '$regValue'. Expected: x == 1"
+                        Status  = "False"
+                    }
+                }
+            }
+            catch [System.Management.Automation.PSArgumentException] {
+                return @{
+                    Message = "Registry value not found."
+                    Status  = "False"
+                }
+            }
+            catch [System.Management.Automation.ItemNotFoundException] {
+                return @{
+                    Message = "Registry key not found."
+                    Status  = "False"
+                }
+            }
+
+            return @{
+                Message = "Compliant"
+                Status  = "True"
+            }
+        }
+    }
+}
+[AuditTest] @{
+    Id   = "1.1.4.1.11 N"
+    Task = "(L1) Ensure 'Restrict ActiveX Install' is set to Enabled  (mse7.exe)"
+    Test = {
+        # new logic: 
+        # - if no Office installed at all -> skip test 
+        # - if Office installed but app not installed -> skip test
+        # - else run test as normal
+
+        if (-not $OfficeInstalled) {
+            return @{
+                Message = "No Office installation detected, skipping test."
+                Status  = "None"
+            }
+        }
+        elseif (-not $installedOfficeApps["MS Script Editor"]) {
+            return @{
+                Message = "Application not installed, skipping test."
+                Status  = "None"
+            }
+        }
+        else {
+            try {
+                $regValue = Get-ItemProperty -ErrorAction Stop `
+                    -Path "Registry::HKEY_LOCAL_MACHINE\Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_RESTRICT_ACTIVEXINSTALL" `
+                    -Name "mse7.exe" `
+                | Select-Object -ExpandProperty "mse7.exe"
 
                 if (($regValue -ne 1)) {
                     return @{
