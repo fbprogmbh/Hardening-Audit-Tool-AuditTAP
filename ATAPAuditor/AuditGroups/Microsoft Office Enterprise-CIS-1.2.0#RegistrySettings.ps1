64cd8b6c95340b0e0e6b56227d23d77ec29a7151
RAW0611 <raw0611@gmail.com>
2025-09-08 11:52:03 +0200

    1.1.4.1.9 order rearranged

--- ATAPAuditor/AuditGroups/Microsoft Office Enterprise-CIS-1.2.0#RegistrySettings.ps1
+++ ATAPAuditor/AuditGroups/Microsoft Office Enterprise-CIS-1.2.0#RegistrySettings.ps1
@@ -6229,8 +6229,8 @@ else {
     }
 }
 [AuditTest] @{
-    Id   = "1.1.4.1.9 K"
-    Task = "(L1) Ensure 'Object Caching Protection' is set to Enabled  (exprwd.exe)"
+    Id   = "1.1.4.1.9 A"
+    Task = "(L1) Ensure 'Object Caching Protection' is set to Enabled  (groove.exe)"
     Test = {
         # new logic: 
         # - if no Office installed at all -> skip test 
@@ -6243,7 +6243,7 @@ else {
                 Status  = "None"
             }
         }
-        elseif (-not $installedOfficeApps["Expression Web"]) {
+        elseif (-not $installedOfficeApps["Groove"]) {
             return @{
                 Message = "Application not installed, skipping test."
                 Status  = "None"
@@ -6253,118 +6253,8 @@ else {
             try {
                 $regValue = Get-ItemProperty -ErrorAction Stop `
                     -Path "Registry::HKEY_LOCAL_MACHINE\Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_OBJECT_CACHING" `
-                    -Name "exprwd.exe" `
-                | Select-Object -ExpandProperty "exprwd.exe"
-
-                if (($regValue -ne 1)) {
-                    return @{
-                        Message = "Registry value is '$regValue'. Expected: x == 1"
-                        Status  = "False"
-                    }
-                }
-            }
-            catch [System.Management.Automation.PSArgumentException] {
-                return @{
-                    Message = "Registry value not found."
-                    Status  = "False"
-                }
-            }
-            catch [System.Management.Automation.ItemNotFoundException] {
-                return @{
-                    Message = "Registry key not found."
-                    Status  = "False"
-                }
-            }
-
-            return @{
-                Message = "Compliant"
-                Status  = "True"
-            }
-        }
-    }
-}
-[AuditTest] @{
-    Id   = "1.1.4.1.9 L"
-    Task = "(L1) Ensure 'Object Caching Protection' is set to Enabled  (msaccess.exe)"
-    Test = {
-        # new logic: 
-        # - if no Office installed at all -> skip test 
-        # - if Office installed but app not installed -> skip test
-        # - else run test as normal
-
-        if (-not $OfficeInstalled) {
-            return @{
-                Message = "No Office installation detected, skipping test."
-                Status  = "None"
-            }
-        }
-        elseif (-not $installedOfficeApps["Access"]) {
-            return @{
-                Message = "Application not installed, skipping test."
-                Status  = "None"
-            }
-        }
-        else {
-            try {
-                $regValue = Get-ItemProperty -ErrorAction Stop `
-                    -Path "Registry::HKEY_LOCAL_MACHINE\Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_OBJECT_CACHING" `
-                    -Name "msaccess.exe" `
-                | Select-Object -ExpandProperty "msaccess.exe"
-
-                if (($regValue -ne 1)) {
-                    return @{
-                        Message = "Registry value is '$regValue'. Expected: x == 1"
-                        Status  = "False"
-                    }
-                }
-            }
-            catch [System.Management.Automation.PSArgumentException] {
-                return @{
-                    Message = "Registry value not found."
-                    Status  = "False"
-                }
-            }
-            catch [System.Management.Automation.ItemNotFoundException] {
-                return @{
-                    Message = "Registry key not found."
-                    Status  = "False"
-                }
-            }
-
-            return @{
-                Message = "Compliant"
-                Status  = "True"
-            }
-        }
-    }
-}
-[AuditTest] @{
-    Id   = "1.1.4.1.9 N"
-    Task = "(L1) Ensure 'Object Caching Protection' is set to Enabled  (mse7.exe)"
-    Test = {
-        # new logic: 
-        # - if no Office installed at all -> skip test 
-        # - if Office installed but app not installed -> skip test
-        # - else run test as normal
-
-        if (-not $OfficeInstalled) {
-            return @{
-                Message = "No Office installation detected, skipping test."
-                Status  = "None"
-            }
-        }
-        elseif (-not $installedOfficeApps["MS Script Editor"]) {
-            return @{
-                Message = "Application not installed, skipping test."
-                Status  = "None"
-            }
-        }
-        else {
-            try {
-                $regValue = Get-ItemProperty -ErrorAction Stop `
-                    -Path "Registry::HKEY_LOCAL_MACHINE\Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_OBJECT_CACHING" `
-                    -Name "mse7.exe" `
-                | Select-Object -ExpandProperty "mse7.exe"
+                    -Name "groove.exe" `
+                | Select-Object -ExpandProperty "groove.exe"
 
                 if (($regValue -ne 1)) {
                     return @{
@@ -6724,6 +6614,61 @@ else {
     }
 }
 [AuditTest] @{
+    Id   = "1.1.4.1.9 H"
+    Task = "(L1) Ensure 'Object Caching Protection' is set to 'Enabled' (winword.exe)"
+    Test = {
+        # new logic: 
+        # - if no Office installed at all -> skip test 
+        # - if Office installed but app not installed -> skip test
+        # - else run test as normal
+
+        if (-not $OfficeInstalled) {
+            return @{
+                Message = "No Office installation detected, skipping test."
+                Status  = "None"
+            }
+        }
+        elseif (-not $installedOfficeApps["Word"]) {
+            return @{
+                Message = "Application not installed, skipping test."
+                Status  = "None"
+            }
+        }
+        else {
+            try {
+                $regValue = Get-ItemProperty -ErrorAction Stop `
+                    -Path "Registry::HKEY_LOCAL_MACHINE\software\microsoft\internet explorer\main\featurecontrol\feature_object_caching" `
+                    -Name "winword.exe" `
+                | Select-Object -ExpandProperty "winword.exe"
+
+                if (($regValue -ne 1)) {
+                    return @{
+                        Message = "Registry value is '$regValue'. Expected: x == 1"
+                        Status  = "False"
+                    }
+                }
+            }
+            catch [System.Management.Automation.PSArgumentException] {
+                return @{
+                    Message = "Registry value not found."
+                    Status  = "False"
+                }
+            }
+            catch [System.Management.Automation.ItemNotFoundException] {
+                return @{
+                    Message = "Registry key not found."
+                    Status  = "False"
+                }
+            }
+
+            return @{
+                Message = "Compliant"
+                Status  = "True"
+            }
+        }
+    }
+}
+[AuditTest] @{
     Id   = "1.1.4.1.9 I"
     Task = "(L1) Ensure 'Object Caching Protection' is set to Enabled  (outlook.exe)"
     Test = {
@@ -6834,8 +6779,8 @@ else {
     }
 }
 [AuditTest] @{
-    Id   = "1.1.4.1.9 A"
-    Task = "(L1) Ensure 'Object Caching Protection' is set to Enabled  (groove.exe)"
+    Id   = "1.1.4.1.9 K"
+    Task = "(L1) Ensure 'Object Caching Protection' is set to Enabled  (exprwd.exe)"
     Test = {
         # new logic: 
         # - if no Office installed at all -> skip test 
@@ -6848,7 +6793,7 @@ else {
                 Status  = "None"
             }
         }
-        elseif (-not $installedOfficeApps["Groove"]) {
+        elseif (-not $installedOfficeApps["Expression Web"]) {
             return @{
                 Message = "Application not installed, skipping test."
                 Status  = "None"
@@ -6858,8 +6803,63 @@ else {
             try {
                 $regValue = Get-ItemProperty -ErrorAction Stop `
                     -Path "Registry::HKEY_LOCAL_MACHINE\Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_OBJECT_CACHING" `
-                    -Name "groove.exe" `
-                | Select-Object -ExpandProperty "groove.exe"
+                    -Name "exprwd.exe" `
+                | Select-Object -ExpandProperty "exprwd.exe"
+
+                if (($regValue -ne 1)) {
+                    return @{
+                        Message = "Registry value is '$regValue'. Expected: x == 1"
+                        Status  = "False"
+                    }
+                }
+            }
+            catch [System.Management.Automation.PSArgumentException] {
+                return @{
+                    Message = "Registry value not found."
+                    Status  = "False"
+                }
+            }
+            catch [System.Management.Automation.ItemNotFoundException] {
+                return @{
+                    Message = "Registry key not found."
+                    Status  = "False"
+                }
+            }
+
+            return @{
+                Message = "Compliant"
+                Status  = "True"
+            }
+        }
+    }
+}
+[AuditTest] @{
+    Id   = "1.1.4.1.9 L"
+    Task = "(L1) Ensure 'Object Caching Protection' is set to Enabled  (msaccess.exe)"
+    Test = {
+        # new logic: 
+        # - if no Office installed at all -> skip test 
+        # - if Office installed but app not installed -> skip test
+        # - else run test as normal
+
+        if (-not $OfficeInstalled) {
+            return @{
+                Message = "No Office installation detected, skipping test."
+                Status  = "None"
+            }
+        }
+        elseif (-not $installedOfficeApps["Access"]) {
+            return @{
+                Message = "Application not installed, skipping test."
+                Status  = "None"
+            }
+        }
+        else {
+            try {
+                $regValue = Get-ItemProperty -ErrorAction Stop `
+                    -Path "Registry::HKEY_LOCAL_MACHINE\Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_OBJECT_CACHING" `
+                    -Name "msaccess.exe" `
+                | Select-Object -ExpandProperty "msaccess.exe"
 
                 if (($regValue -ne 1)) {
                     return @{
@@ -6944,8 +6944,8 @@ else {
     }
 }
 [AuditTest] @{
-    Id   = "1.1.4.1.9 H"
-    Task = "(L1) Ensure 'Object Caching Protection' is set to 'Enabled' (winword.exe)"
+    Id   = "1.1.4.1.9 N"
+    Task = "(L1) Ensure 'Object Caching Protection' is set to Enabled  (mse7.exe)"
     Test = {
         # new logic: 
         # - if no Office installed at all -> skip test 
@@ -6958,7 +6958,7 @@ else {
                 Status  = "None"
             }
         }
-        elseif (-not $installedOfficeApps["Word"]) {
+        elseif (-not $installedOfficeApps["MS Script Editor"]) {
             return @{
                 Message = "Application not installed, skipping test."
                 Status  = "None"
@@ -6967,9 +6967,9 @@ else {
         else {
             try {
                 $regValue = Get-ItemProperty -ErrorAction Stop `
-                    -Path "Registry::HKEY_LOCAL_MACHINE\software\microsoft\internet explorer\main\featurecontrol\feature_object_caching" `
-                    -Name "winword.exe" `
-                | Select-Object -ExpandProperty "winword.exe"
+                    -Path "Registry::HKEY_LOCAL_MACHINE\Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_OBJECT_CACHING" `
+                    -Name "mse7.exe" `
+                | Select-Object -ExpandProperty "mse7.exe"
 
                 if (($regValue -ne 1)) {
                     return @{
